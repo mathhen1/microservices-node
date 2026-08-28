10d15330cf9767976e701e4f3eb44588e65cd839
mathen <math.z.mhj@gmail.com>
2026-08-27 22:04:48 -0300

    feat: create mock repository for orders

---
+++ src/orders/repository/orderRepository.js
@@ -0,0 +1,6 @@
+export const orders = [
+    {id: 1, name: "burger king", price: 25, userId: 1},
+    {id: 2, name: "mc donalds", price: 35, userId: 1},
+    {id: 3, name: "galettus", price: 78, userId: 2},
+    {id: 4, name: "giraffas", price: 50, userId: 1}
+]
