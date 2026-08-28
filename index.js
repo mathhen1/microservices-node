9d5741822c9e29faf943f53b191e21519577e627
mathen <math.z.mhj@gmail.com>
2026-08-27 22:03:30 -0300

    feat: create new service for orders

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import "dotenv/config"
 import express from "express"
 import { userRouter } from "./src/users/routes/usersRoutes.js"
+import { ordersRouter } from "./src/orders/routes/orderRoutes.js"
 
 const app = express()
 const port = process.env.PORT
@@ -8,6 +9,7 @@ const port = process.env.PORT
 app.use(express.json())
 
 app.use("/users", userRouter)
+app.use("/orders", ordersRouter)
 
 app.listen(port, () => {
     console.log("Server rodando; Porta:", port)
