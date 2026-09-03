740314765a127c2578ba7c66d199b588c6448e79
mathen <math.z.mhj@gmail.com>
2026-09-03 14:45:31 -0300

    feat: implements route for create order method

--- src/orders/routes/orderRoutes.js
+++ src/orders/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getOrder, getOrders, getOrdersByUser } from "../controllers/orderController.js";
+import { createOrder, getOrder, getOrders, getOrdersByUser } from "../controllers/orderController.js";
 
 const ordersRouter = Router()
 
 ordersRouter.get("/", getOrders)
 ordersRouter.get("/:id", getOrder)
 ordersRouter.get("/users/:id", getOrdersByUser)
+ordersRouter.post("/", createOrder)
 
 export { ordersRouter }
