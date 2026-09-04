c1355abf807b9f0c62a95f53d373b348866df7e2
mathen <math.z.mhj@gmail.com>
2026-09-03 23:53:35 -0300

    feat: create func for consumer message for orders created

---
+++ src/orders/messaging/consumerOrderCreated.js
@@ -0,0 +1,26 @@
+import amqp from "amqplib"
+
+export const consumerOrderCreated = async () => {
+    try {
+        const connection = await amqp.connect("amqp://localhost")
+        const channel = await connection.createChannel()
+
+        const exchange = "order_messages"
+        await channel.assertExchange(exchange, "topic", { durable: true })
+
+        const queue = "notification_service_orders"
+        await channel.assertQueue(queue, { durable: "true" })
+        await channel.bindQueue(queue, exchange, "order.created")
+
+        channel.consume(queue, (msg) => {
+            if (msg) {
+                const order = JSON.parse(msg.content.toString())
+                console.log(`Enviando confirmação do pedido! Pedido:${order.id}`)
+                channel.ack(msg)
+            }
+        })
+
+    } catch (err) {
+        console.error("Error consumer message: ", err)
+    }
+}
