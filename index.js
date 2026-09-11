933761c1fae3fe9658145016fe683340b06fc815
mathen <math.z.mhj@gmail.com>
2026-09-11 01:08:30 -0300

    refactor: update server base to api gateway for the microservices

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 import "dotenv/config"
 import express from "express"
-import { userRouter } from "./src/users/routes/usersRoutes.js"
-import { ordersRouter } from "./src/orders/routes/orderRoutes.js"
-import { notificationsRouter } from "./src/notifications/routes/notificationRoutes.js"
+import { createProxyMiddleware } from "http-proxy-middleware"
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.API_GATEWAY_PORT
+
+const services = {
+    users: `http://127.0.0.1:${process.env.USERS_SERVICE_PORT}`,
+    orders: `http://127.0.0.1:${process.env.ORDERS_SERVICE_PORT}`,
+    notifications: `http://127.0.0.1:${process.env.NOTIFICATIONS_SERVICE_PORT}`
+}
+
+const usersServiceProxy = createProxyMiddleware({
+    target: services.users,
+    changeOrigin: true,
+    pathRewrite: (path) => {
+        return "/users" + path
+    },
+    logger: console
+})
+
+const ordersServiceProxy = createProxyMiddleware({
+    target: services.orders,
+    changeOrigin: true,
+    pathRewrite: (path) => {
+        return "/orders" + path
+    },
+    logger: console
+})
+
+const notificationsServiceProxy = createProxyMiddleware({
+    target: services.notifications,
+    changeOrigin: true,
+    pathRewrite: (path) => {
+        return "/notifications" + path
+    },
+    logger: console
+})
+
+app.use("/api/users", usersServiceProxy)
+app.use("/api/orders", ordersServiceProxy)
+app.use("/api/notifications", notificationsServiceProxy)
 
 app.use(express.json())
 
-app.use("/users", userRouter)
-app.use("/orders", ordersRouter)
-app.use("/notifications", notificationsRouter)
-
 app.listen(port, () => {
-    console.log("Server rodando; Porta:", port)
-    consumerOrderCreated()
+    console.log("API Gateway rodando; Porta:", port)
 })
